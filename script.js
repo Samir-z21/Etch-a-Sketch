3974ce3ea83a96a0a4243486d2c75d75e2e48c7a
Samir <samir_zulfiqar@yahoo.com>
2023-07-19 19:05:07 -0400

    clean code, further improved style and attempted an eventListener for the grid

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 // Reference to body
 const container = document.querySelector("#gridContainer");
 
+// declaring variables outside functions
+let singleGrid;
+let column;
+let wholeGrid;
+let oneColumn;
+
+// create grid and append it
+let Grid = createGrid ();
+container.appendChild(Grid)
+
+// function to create a column with 16 single grids
 function createColumn () {
-  let column = document.createElement("div");
+  column = document.createElement("div");
   column.className = 'columns';
    for (let i = 0; i <16; i++ ){
-    let singleGrid = document.createElement("div");
+    singleGrid = document.createElement("div");
     singleGrid.className = 'singleGrids';
     column.appendChild(singleGrid);
 }
 return column;
 }
 
+// funciton to create a grid with 16 columns
 function createGrid() {
-  let wholeGrid = document.createElement("div");
+  wholeGrid = document.createElement("div");
   wholeGrid.className = 'wholeGrids';
    for (let i = 0; i <16; i++) {
-    let oneColumn = createColumn ();
+    oneColumn = createColumn ();
     wholeGrid.appendChild(oneColumn);
    }
    
    return wholeGrid;
 }
-let Grid = createGrid ();
-container.appendChild(Grid)
+
+// function to leave a trail on the grids
+singleGrid.addEventListener("mouseover", function(){
+    singleGrid.style.backgroundColor = "grey"
+})
