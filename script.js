33379d8860e9f2c34f36192ab9781f65a13efa57
Samir <samir_zulfiqar@yahoo.com>
2023-07-24 16:06:16 -0400

    added hovering feature over square divs

--- script.js
+++ script.js
@@ -2,40 +2,49 @@
 const container = document.querySelector("#gridContainer");
 
 // declaring variables outside functions
-let singleGrid;
+let singleSquare;
 let column;
 let wholeGrid;
-let oneColumn;
+let squares = [];
 
 // create grid and append it
-let Grid = createGrid ();
-container.appendChild(Grid)
-
-// function to create a column with 16 single grids
-function createColumn () {
-  column = document.createElement("div");
-  column.className = 'columns';
-   for (let i = 0; i <16; i++ ){
-    singleGrid = document.createElement("div");
-    singleGrid.className = 'singleGrids';
-    column.appendChild(singleGrid);
-}
-return column;
-}
+wholeGrid = createGrid ();
+container.appendChild(wholeGrid)
 
 // funciton to create a grid with 16 columns
 function createGrid() {
   wholeGrid = document.createElement("div");
   wholeGrid.className = 'wholeGrids';
    for (let i = 0; i <16; i++) {
-    oneColumn = createColumn ();
-    wholeGrid.appendChild(oneColumn);
+    column = createColumn ();
+    wholeGrid.appendChild(column);
    }
    
    return wholeGrid;
 }
+// function to create a column with 16 single grids
+function createColumn () {
+  column = document.createElement("div");
+  column.className = 'columns';
+   for (let i = 0; i <16; i++ ){
+    singleSquare = document.createElement("div");
+    singleSquare.className = 'singleSquares';
+    column.appendChild(singleSquare);
+    squares.push(singleSquare);
+}
+return column
+}
 
-// function to leave a trail on the grids
-singleGrid.addEventListener("mouseover", function(){
-    singleGrid.style.backgroundColor = "grey"
-})
+
+
+// code for changing square colors while hovering
+function changeColor(square) {
+  square.style.backgroundColor = "grey";
+}
+
+squares.forEach((square) => {
+  square.addEventListener('mousemove', () => {
+    changeColor(square)
+  });
+});
+
